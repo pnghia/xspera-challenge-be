1d98989ebeed479bd92e4961e3ae2dfe1946e148
nghiaphamt <nghia.phamtrong@nashtechglobal.com>
2019-10-02 02:29:46 +0700

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -8,6 +9,7 @@ const reviewRouter = require('./components/review');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
